5f0ce36613e1b07319f37d12e79c4a9c4dbc1316
Martin Kubat <martin.kubat@coex.cz>
2014-10-21 13:51:19 +0200

    cleanup

--- openode/models/user.py
+++ openode/models/user.py
@@ -469,7 +469,6 @@ class Organization(models.Model):
                 crop="center" if crop else None
             ).url
         except (IOError, ThumbnailError), e:
-            print e
             logging.error(repr({
                 "error": e,
                 "method": "get_logo_url",
