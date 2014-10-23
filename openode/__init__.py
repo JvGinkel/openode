f16dbd41c15e820c8a97a314c040c42a78ab8355
Martin Kubat <martin.kubat@coex.cz>
2014-10-23 11:10:41 +0200

    tag 1.1.2

--- openode/__init__.py
+++ openode/__init__.py
@@ -12,7 +12,7 @@ import platform
 
 # from django.conf import settings
 
-VERSION = (1, 1, 1)
+VERSION = (1, 1, 2)
 
 #keys are module names used by python imports,
 #values - the package qualifier to use for pip
